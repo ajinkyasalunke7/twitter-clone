ea17d513867c891be569398f84bbab60df66e545
ajinkyasalunke7 <ajinkyasalunke90@outlook.com>
2024-07-12 11:35:36 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        <h1 className="text-white">Hello World!</h1>
+      </div>
+    </main>
+  );
+}
