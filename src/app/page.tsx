25025eb505560f6be9f67707a6028f6d8af3b2d8
ajinkyasalunke7 <ajinkyasalunke90@outlook.com>
2024-07-24 00:05:46 +0530

    Modal Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
-  return (
-    <main className="">
-      <div>
-        <h1 className="text-white">Hello World!</h1>
-      </div>
-    </main>
-  );
+    return (
+        <>
+            <Header showBackArrow label="Home" />
+        </>
+    );
 }
